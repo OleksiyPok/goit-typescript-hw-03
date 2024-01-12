9a75764dd5a7f3d0b1e7f99a20a5454134fd725d
Oleksiy Pokormyakho <116438996+OleksiyPok@users.noreply.github.com>
2024-01-12 21:34:17 +0100

    fin 01

--- src/4.ts
+++ src/4.ts
@@ -1,11 +1,58 @@
+class Key {
+  constructor(
+    private signature: number = Math.floor(
+      // Math.random() * (10000 - 1000) + 1000
+      Math.random() * (5 - 1) + 1
+    )
+  ) {}
+
+  getSignature() {
+    return this.signature;
+  }
+}
+
+class Person {
+  constructor(private key: Key = key) {}
+
+  getKey() {
+    return this.key;
+  }
+}
+
+abstract class House {
+  constructor(
+    protected door: boolean,
+    protected key: object,
+    protected tenants: object[]
+  ) {}
+
+  public comeIn(person: object): void {
+    if (this.door) {
+      this.tenants.push(person);
+    }
+  }
+
+  abstract openDoor(key: Key): void;
+}
+
+class MyHouse extends House {
+  openDoor(key: Key): void {
+    if (key === this.key) {
+      this.door = true;
+    }
+  }
+  getTenants() {
+    return this.tenants;
+  }
+}
+
 const key = new Key();
 
-const house = new MyHouse(key);
 const person = new Person(key);
 
-house.openDoor(person.getKey());
+const myHouse = new MyHouse(false, key, []);
 
-house.comeIn(person);
-
+myHouse.openDoor(person.getKey());
+myHouse.comeIn(person);
 
 export {};
